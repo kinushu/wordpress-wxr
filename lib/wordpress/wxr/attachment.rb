19c23f47d7b5e3c4ad98f47f9e66aea1752c4a3d
Robert James Kaes <rkaes@ePublishing.com>
2017-03-16 20:24:14 -0400

    ASCIIify URLs for Attachments
    The `URI` module does not support UTF-8 characters.  Replace any UTF-8
    characters (and spaces if present) with ASCII equivalent encoding to
    feed into the `URI` conversion function without error.

--- lib/wordpress/wxr/attachment.rb
+++ lib/wordpress/wxr/attachment.rb
@@ -1,11 +1,28 @@
 require 'wordpress/wxr/item'
+require 'cgi'
 require 'uri'
 
 module Wordpress
   class WXR
     class Attachment < Item
+      # Internal: Add methods to normalize potential utf-8 encodings into
+      # standard ASCII escaping to feed into URI which dies on utf-8 characters.
+      module Refinements
+        refine String do
+          def replace_spaces_with_hex_encoding
+            gsub(/\s/, '%20')
+          end
+
+          def percent_encode_non_ascii
+            encode(Encoding.find('ASCII'), invalid: :replace, fallback: ->(char) { CGI.escape(char) })
+          end
+        end
+      end
+
+      using Refinements
+
       def url
-        URI(node.xpath('wp:attachment_url').text)
+        URI(String(node.xpath('wp:attachment_url').text).replace_spaces_with_hex_encoding.percent_encode_non_ascii)
       end
     end
   end
